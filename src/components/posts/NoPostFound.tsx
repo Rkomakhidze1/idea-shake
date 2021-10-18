640969d2a5e5c083ac598482f068734af7b9a7ae
Rkomakhidze1 <rezokomakhidze1@gmail.com>
2021-10-18 12:45:49 +0400

    fixed minor styles

--- src/components/posts/NoPostFound.tsx
+++ src/components/posts/NoPostFound.tsx
@@ -4,7 +4,7 @@ import classes from './NoPostsFound.module.css';
 
 const NoPostsFound = () => {
   return (
-    <div className={classes.noPosts}>
+    <div className={classes.noposts}>
       <p>No Posts found!</p>
       <Link className="btn" to="/new-post">
         Add a Post
